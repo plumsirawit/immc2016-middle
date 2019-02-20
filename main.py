b17cc066ff660619ec26a14b37d4a17f1e5d5e78
Sirawit Pongnakintr <s6007589@mwit.ac.th>
2019-02-20 18:38:00 +0700

    Add calculate function

--- main.py
+++ main.py
@@ -12,9 +12,25 @@ with open('immc2016-data.csv') as f:
 		cdat[4] = int(cdat[4])
 		dat.append(cdat)
 print(dat)
+def calculate(window):
+	
 def simulate(c1, c2, c3):
 	qs = [0]
+	country_win_count = {}
+	for stats in dat:
+		country_win_count[stats[3]] = 0
+	last_winner_country = ''
+	last_winner_name = ''
 	for i in range(1,len(dat)):
+		slope = stats.linregress(list(range(i)),qs)[0]
+		current_window = []
+		for j in country_list:
+			buf = country_win_count[j] + c3
+			current_window.append((j,buf,'Anonymous'))
+			if last_winner_country == j:
+				buf += c1 * streak
+			current_window.append((j,buf,last_winner_name))
+		calculate(current_window)
 		qs.append(qs[-1] + dat[i][4])
-		slope = stats.linregress(list(range(i+1),qs))[0]
+		country_win_count[dat[i][3]] += 1
 		
